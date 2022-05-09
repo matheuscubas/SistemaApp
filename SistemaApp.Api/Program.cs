acd5aaeab17039471cbdb8873de0b6a9551c3cb6
matheus_vidal <maatheusviidal@hotmail.com>
2022-05-09 16:04:54 -0300

    Added serilog

--- SistemaApp.Api/Program.cs
+++ SistemaApp.Api/Program.cs
@@ -1,6 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
